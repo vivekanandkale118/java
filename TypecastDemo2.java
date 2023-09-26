3cee0e7959b5d4e7ecf86ee248d6a0431d277fba
vivekanandkale118 <146057119+vivekanandkale118@users.noreply.github.com>
2023-09-26 10:53:15 +0530

    Add files via upload

---
+++ TypecastDemo2.java
@@ -0,0 +1,11 @@
+public class TypecastDemo2 {
+public static void main(String[]args)
+{
+double d = 166.6;
+long l= (long)d;
+int i = (int)l;
+System.out.println("Before conversion:"+d);
+System.out.println("After conversion into long type:"+l);
+System.out.println("After conversion into int type:"+i);
+ }
+} 
